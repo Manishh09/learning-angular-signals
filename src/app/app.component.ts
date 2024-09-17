ab634a14b2840cf29f50da1c501edeb2b75dc742
Manish Boge (SX/EDC2) <manish.boge@in.bosch.com>
2024-09-17 11:14:42 +0530

    chg: updated app component html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'learning-angular-signals';
+  title = 'Welcome to Learning Angular Signals Session';
 }
